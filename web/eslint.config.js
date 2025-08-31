01ca6184de03795a53d105a49475f244a43342c5
quangtran666 <quangprox121@gmail.com>
2025-08-31 12:26:08 +0700

    Setup React Vite-based web project with ESLint, Husky, lint-staged, and Prettier

---
+++ web/eslint.config.js
@@ -0,0 +1,61 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+export default tseslint.config([
+  globalIgnores(["dist", "node_modules", "build", "src/components/ui"]),
+  eslintPluginUnicorn.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      eslintPluginReact.configs.flat.recommended,
+      eslintPluginReact.configs.flat["jsx-runtime"],
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      eqeqeq: ["error", "always"],
+      curly: ["error", "multi-or-nest"],
+      "prefer-template": ["error"],
+      "prefer-arrow-callback": ["error"],
+      "object-shorthand": ["error", "always"],
+      "arrow-body-style": ["error", "as-needed"],
+
+      "unicorn/filename-case": ["error", { case: "kebabCase" }],
+      "unicorn/prevent-abbreviations": [
+        "error",
+        {
+          checkFilenames: false,
+        },
+      ],
+      "unicorn/prefer-node-protocol": "off",
+
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+]);
